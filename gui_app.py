b4e4740547ac2edba8ef2e304aaa491e9f69b6b7
Mario Suero <159667524+K1lluaZk@users.noreply.github.com>
2024-12-24 00:05:31 -0400

    Actualizacion v2
    Faltan algunos cambios solamente

--- gui_app.py
+++ gui_app.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from model.pelicula_dao import *
 
+
+
 def barra_menu(root):
     barra_menu = tk.Menu(root)
     root.config(menu = barra_menu, width = 300, height = 300)
@@ -14,9 +16,21 @@ def barra_menu(root):
     menu_inicio.add_command(label = "Actualizar Registro")
     menu_inicio.add_command(label = "Salir", command = exit)
     
-    barra_menu.add_cascade(label = "Consultas")
-    barra_menu.add_cascade(label = "Configuracion")
-    barra_menu.add_cascade(label = "Ayuda")
+    menu_consultas = tk.Menu(barra_menu, tearoff = 0)
+    barra_menu.add_cascade(label = "Consultas", menu = menu_consultas)
+    
+    menu_consultas.add_command(label = "Listar Registros")
+    menu_consultas.add_command(label = "Exportar Registros")
+    menu_consultas.add_command(label = "Estadisticas")
+    
+    menu_ayuda = tk.Menu(barra_menu, tearoff = 0)
+    barra_menu.add_cascade(label = "Ayuda", menu = menu_ayuda)
+    
+    menu_ayuda.add_command(label = "Manual")
+    menu_ayuda.add_command(label = "Contacto")
+    
+    
+    
 
     
 
@@ -25,8 +39,10 @@ class Frame(tk.Frame):
         super().__init__(root, width = 480, height = 320,)
         self.root = root
         self.pack()
+        self.id_pelicula = None
         #(Acordar tocar despues)
         #self.config( bg = "blue")
+        
         
         self.campos_pelicula()
         self.deshabilitar_campos()
@@ -81,6 +97,8 @@ class Frame(tk.Frame):
         self.boton_cancelar.config(state="normal") 
         
     def deshabilitar_campos(self):
+        self.id_pelicula = None
+        
         self.mi_nombre.set("")
         self.mi_duracion.set("")
         self.mi_genero.set("")
@@ -94,28 +112,90 @@ class Frame(tk.Frame):
         
     def guardar_datos(self):
         
+        
+        pelicula = Pelicula(
+            self.mi_nombre.get(),
+            self.mi_duracion.get(),
+            self.mi_genero.get()
+        )
+        
+        #####################################
+        if self.id_pelicula == None:
+           guardar(pelicula)
+        else:
+           editar(pelicula, self.id_pelicula)
+
+        
+        self.tabla_peliculas()
+        
         self.deshabilitar_campos()
         
         
     def tabla_peliculas(self):
-   
+        
+        self.lista_peliculas = listar()
+        self.lista_peliculas.reverse()
         self.tabla =ttk.Treeview(self, 
         column = ("Nombre", "Duracion", "Genero"))
-        self.tabla.grid(row = 4, column = 0, columnspan = 4)
+        self.tabla.grid(row = 4, column = 0, columnspan = 4, sticky = "nse")
+        
+        self.scroll = ttk.Scrollbar(self, orient = "vertical", command = self.tabla.yview)
+        self.scroll.grid(row = 4, column = 4, sticky = "nse")
+        self.tabla.configure(yscrollcommand = self.scroll.set)
         
         self.tabla.heading("#0", text = "ID")
         self.tabla.heading("#1", text = "Nombre")
         self.tabla.heading("#2", text = "Duración")
         self.tabla.heading("#3", text = "Genero")
 
-        self.tabla.insert("",0, text = "1", values = ("Los Vengadores" "2.35", "Acción"))
+        for p in self.lista_peliculas:
+            self.tabla.insert("",0, text = p[0], values = (p[1], p[2], p[3]))
 
-        self.boton_editar = tk.Button(self, text="Editar", command = self.habilitar_campos)
+        self.boton_editar = tk.Button(self, text="Editar", command = self.editar_datos)
         self.boton_editar.config(width = 20, cursor = "hand2",  activebackground = "#5E5ADD", font = ("Arial", 12, "bold"), fg = "white", bg = "#0C087A")
         self.boton_editar.grid(row = 5, column = 0,  padx = 10, pady = 10)
         
                  
-        self.boton_eliminar = tk.Button(self, text="Eliminar", command = self.deshabilitar_campos)
+        self.boton_eliminar = tk.Button(self, text="Eliminar", command = self.eliminar_datos)
         self.boton_eliminar.config(width = 20, cursor = "hand2", activebackground = "#F76C6D", font = ("Arial", 12, "bold"), fg = "white", bg = "#E4080A")
         self.boton_eliminar.grid(row = 5, column = 1,  padx = 10, pady = 10)
             
+   ######################################################     
+    def editar_datos(self):
+        try: 
+            self.id_pelicula = self.tabla.item(self.tabla.selection())["text"]
+            self.nombre_pelicula = self.tabla.item(
+                self.tabla.selection())["values"][0]
+            self.duracion_pelicula = self.tabla.item(
+                self.tabla.selection())["values"][1]
+            self.genero_pelicula = self.tabla.item(
+                self.tabla.selection())["values"][2]
+
+            self.habilitar_campos()
+            
+            
+            self.entry_nombre.delete(0, tk.END)
+            self.entry_duracion.delete(0, tk.END)
+            self.entry_genero.delete(0, tk.END)
+
+            self.entry_nombre.insert(0, self.nombre_pelicula)
+            self.entry_duracion.insert(0, self.duracion_pelicula)
+            self.entry_genero.insert(0, self.genero_pelicula)
+
+        except:
+            titulo = "Edición De Datos"
+            mensaje = "No Ha Seleccionado Ningun Registro"
+            messagebox.showerror(titulo, mensaje)
+            
+    def eliminar_datos(self):
+        try:
+            self.id_pelicula = self.tabla.item(self.tabla.selection())["text"]
+            eliminar(self.id_pelicula)
+
+
+            self.tabla_peliculas()
+            self.id_pelicula = None
+        except:
+            titulo = "Eliminar Un Registro"
+            mensaje = "No Ha Seleccionado Ningun Registro"
+            messagebox.showerror(titulo, mensaje)
--- pelicula_dao.py
+++ pelicula_dao.py
@@ -62,3 +62,47 @@ def guardar(pelicula):
         titulo = "Conexion Al Registro"
         mensaje = "La Tabla Peliculas No Esta Creada En La Base De Datos"
         messagebox.showerror(titulo, mensaje)
+        
+def listar():
+    conexion = ConexionDB()
+
+    lista_peliculas = []
+    sql = "SELECT * FROM peliculas"
+
+    try: 
+        conexion.cursor.execute(sql)
+        lista_peliculas = conexion.cursor.fetchall()
+        conexion.cerrar()
+    except:
+        titulo = "Conexion Al Registro"
+        mensaje = "Crea La Tabla En La Base De Datos"
+        messagebox.showwarning(titulo, mensaje)
+
+    return lista_peliculas
+
+
+def editar(pelicula, id_pelicula):
+    conexion = ConexionDB()
+   
+    sql = f"""UPDATE peliculas SET nombre = "{pelicula.nombre}", duracion = "{pelicula.duracion}", genero = "{pelicula.genero}" WHERE id = {id_pelicula}"""
+
+    try: 
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+
+    except:
+        titulo = "Edición De Datos"
+        mensaje = "No Se Ha Podido Editar Este Registro"
+        messagebox.showerror(titulo, mensaje)
+        
+def eliminar(id_pelicula):
+    conexion = ConexionDB()
+    sql = f"DELETE  FROM peliculas WHERE id = {id_pelicula}"
+
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+    except:
+        titulo = "Eliminar Datos"
+        mensaje = "No Se Pudo Eliminar El Registro"
+        messagebox.showerror(titulo, mensaje)
